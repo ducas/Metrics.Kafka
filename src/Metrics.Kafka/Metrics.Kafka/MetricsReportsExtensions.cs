5a1edb65151479905ffe672c612fb6d62ace479f
Ducas Francis <ducas@ducasfrancis.com>
2016-03-16 12:58:40 +1100

    simplify initialisation of reported with extension method

---
+++ src/Metrics.Kafka/Metrics.Kafka/MetricsReportsExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Metrics.Reports;
+
+namespace Metrics.Kafka
+{
+    public static class MetricsReportsExtensions
+    {
+        public static MetricsReports WithKafka(this MetricsReports reports, string zookeeperConnect, string topicName, TimeSpan interval)
+        {
+            return reports.WithReport(new TopicReporter(zookeeperConnect, topicName), interval);
+        }
+    }
+}
